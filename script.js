c9a3141055c8cc597f922b896bed734d6ddab811
Omar05112 <omarhamaini1@gmail.com>
2025-10-18 02:26:58 +0100

    Add and test some necessary functions

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-function add(a,b){
-    return a+b;
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+function operate(operator, firstNumber, secondNumber) {
+  let result = 0;
+  function add(firstNumber, secondNumber) {
+    result = firstNumber + secondNumber;
+  }
+  function substract(firstNumber, secondNumber) {
+    result = firstNumber - secondNumber;
+  }
+  function multiply(firstNumber, secondNumber) {
+    result = firstNumber * secondNumber;
+  }
+  function divide(firstNumber, secondNumber) {
+    result = firstNumber / secondNumber;
+  }
+  switch (operator) {
+    case "-":
+      substract(firstNumber, secondNumber);
+      break;
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "*":
+      multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      divide(firstNumber, secondNumber);
+      break;
+  }
+  return result;
 }
-function substract(a, b){
-    return a-b;
-}
-function multiply(a, b){
-    return a*b;
-}
-function divide(a, b){
-    return a/b;
-}
+//   if (operator == "-") {
+//     substract(firstNumber, secondNumber);
+//     return result;
+//   } else if (operator == "+") {
+//     add(firstNumber, secondNumber);
+//     return result;
+//   } else if (operator == "*") {
+//     multiply(firstNumber, secondNumber);
+//     return result;
+//   } else if (operator == "/") {
+//     divide(firstNumber, secondNumber);
+//     return result;
+//   }
+
+console.log(operate("*", 9, 14));
