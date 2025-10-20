c6013b44e598603eb79e719b871c79273dfc6ef7
Omar05112 <omarhamaini1@gmail.com>
2025-10-20 23:17:21 +0100

    Add numbers being displayed on click

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 let firstNumber = 0;
-let operator = "";
+
 let secondNumber = 0;
+
+let number = document.getElementsByClassName("number");
+
+let userInput = document.getElementById("result");
+let clear = document.getElementById("clear");
+let operator = document.getElementsByClassName("operator");
+userInput.style.color = "white";
+userInput.style.fontSize = "16px";
+userInput.style.textAlign = "right";
+userInput.style.fontFamily = "Arial";
+userInput.textContent = "0";
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function (e) {
+    userInput.style.color = "white";
+    userInput.style.fontSize = "16px";
+    userInput.style.textAlign = "right";
+    userInput.style.fontFamily = "Arial";
+    if (userInput.textContent == "0") {
+      userInput.textContent = number[i].textContent;
+      firstNumber = userInput.textContent;
+    } else if (userInput.textContent.length >= 1) {
+      userInput.textContent = userInput.textContent.concat(
+        number[i].textContent
+      );
+      firstNumber = userInput.textContent;
+    } else {
+      userInput.textContent = number[i].textContent;
+      firstNumber = userInput.textContent;
+    }
+    console.log(
+      `traversed through the function, i is ${i}, character length is ${userInput.textContent.length}, firstNumber inside the eventlistener is ${firstNumber}`
+    );
+  });
+}
+
+
+// console.log(`juh`);
+// console.log(`firstNumber right now is ${firstNumber}`);
+
+
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function (e) {
+    userInput.style.color = "white";
+    userInput.style.fontSize = "16px";
+    userInput.style.textAlign = "right";
+    userInput.style.fontFamily = "Arial";
+
+    
+
+  });
+}
+
+clear.addEventListener("click", (e) => {
+  userInput.innerHTML = "0";
+});
 
 function operate(operator, firstNumber, secondNumber) {
   let result = 0;
