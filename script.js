9c49c412454308400ede4692039c2be92c4b8f9b
Omar05112 <omarhamaini1@gmail.com>
2025-10-22 03:21:57 +0100

    Rework most of the .js, add new functions

--- script.js
+++ script.js
@@ -1,91 +1,84 @@
-let firstNumber = 0;
+let firstNumber = "0";
+let mathOperator = "";
+let secondNumber = "";
 
-let secondNumber = 0;
+let number = document.querySelectorAll(".number");
 
-let number = document.getElementsByClassName("number");
-
-let userInput = document.getElementById("result");
+let display = document.getElementById("result");
 let clear = document.getElementById("clear");
-let operator = document.getElementsByClassName("operator");
-userInput.style.color = "white";
-userInput.style.fontSize = "16px";
-userInput.style.textAlign = "right";
-userInput.style.fontFamily = "Arial";
-userInput.textContent = "0";
-for (let i = 0; i < number.length; i++) {
-  number[i].addEventListener("click", function (e) {
-    userInput.style.color = "white";
-    userInput.style.fontSize = "16px";
-    userInput.style.textAlign = "right";
-    userInput.style.fontFamily = "Arial";
-    if (userInput.textContent == "0") {
-      userInput.textContent = number[i].textContent;
-      firstNumber = userInput.textContent;
-    } else if (userInput.textContent.length >= 1) {
-      userInput.textContent = userInput.textContent.concat(
-        number[i].textContent
-      );
-      firstNumber = userInput.textContent;
-    } else {
-      userInput.textContent = number[i].textContent;
-      firstNumber = userInput.textContent;
-    }
-    console.log(
-      `traversed through the function, i is ${i}, character length is ${userInput.textContent.length}, firstNumber inside the eventlistener is ${firstNumber}`
-    );
+let operator = document.querySelectorAll(".operator");
+
+function newNumber() {
+  number.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      if (!mathOperator) {
+        if (display.textContent == "0") {
+          display.textContent = button.textContent;
+        } else {
+          display.textContent += button.textContent;
+        }
+        firstNumber = display.textContent;
+      } else {
+        display.textContent += button.textContent;
+        secondNumber = display.textContent;
+      }
+      displayStructure();
+    });
   });
 }
 
-
-// console.log(`juh`);
-// console.log(`firstNumber right now is ${firstNumber}`);
-
-
-for (let i = 0; i < operator.length; i++) {
-  operator[i].addEventListener("click", function (e) {
-    userInput.style.color = "white";
-    userInput.style.fontSize = "16px";
-    userInput.style.textAlign = "right";
-    userInput.style.fontFamily = "Arial";
-
-    
-
+function currentOperator() {
+  operator.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      mathOperator = button.textContent;
+      displayStructure();
+    });
   });
 }
 
-clear.addEventListener("click", (e) => {
-  userInput.innerHTML = "0";
-});
-
-function operate(operator, firstNumber, secondNumber) {
-  let result = 0;
-  function add(firstNumber, secondNumber) {
-    result = firstNumber + secondNumber;
-  }
-  function substract(firstNumber, secondNumber) {
-    result = firstNumber - secondNumber;
-  }
-  function multiply(firstNumber, secondNumber) {
-    result = firstNumber * secondNumber;
-  }
-  function divide(firstNumber, secondNumber) {
-    result = firstNumber / secondNumber;
-  }
-  switch (operator) {
-    case "-":
-      substract(firstNumber, secondNumber);
-      break;
-    case "+":
-      add(firstNumber, secondNumber);
-      break;
-    case "*":
-      multiply(firstNumber, secondNumber);
-      break;
-    case "/":
-      divide(firstNumber, secondNumber);
-      break;
-  }
-  return result;
+function clearDisplay() {
+  clear.addEventListener("click", (e) => {
+    display.innerHTML = "0";
+  });
 }
 
-console.log(operate("*", 9, 14));
+newNumber();
+currentOperator();
+clearDisplay();
+
+function displayStructure() {
+  display.textContent = `${firstNumber}${mathOperator}${secondNumber}`;
+}
+
+// function operate(operator, firstNumber, secondNumber) {
+//   let result = 0;
+//   function add(firstNumber, secondNumber) {
+//     result = firstNumber + secondNumber;
+//   }
+//   function substract(firstNumber, secondNumber) {
+//     result = firstNumber - secondNumber;
+//   }
+//   function multiply(firstNumber, secondNumber) {
+//     result = firstNumber * secondNumber;
+//   }
+//   function divide(firstNumber, secondNumber) {
+//     result = firstNumber / secondNumber;
+//   }
+//   switch (operator) {
+//     case "-":
+//       substract(firstNumber, secondNumber);
+//       break;
+//     case "+":
+//       add(firstNumber, secondNumber);
+//       break;
+//     case "*":
+//       multiply(firstNumber, secondNumber);
+//       break;
+//     case "/":
+//       divide(firstNumber, secondNumber);
+//       break;
+//   }
+//   return result;
+// }
+
+// console.log(operate("*", 9, 14));
